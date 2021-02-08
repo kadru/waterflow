d91efbd5509f361181db4fb2cdde4650fe8c03c1
Oscar Rivas <orivas155@gmail.com>
2021-02-07 19:51:21 -0600

    added sengrid to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,11 +112,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
-    domain: ENV['HOST'],
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
+    user_name: ENV['MAILER_USERNAME'],
+    password: ENV['MAILER_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
