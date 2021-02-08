22f5868eaf598eb56ecc5f352aab5d604e50524c
Oscar Rivas <orivas155@gmail.com>
2021-02-07 20:00:25 -0600

    default queue name for mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@
 
 # Main mailer
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@waterflowapp.com'
+  default from: 'noreply@waterflowapp.com', deliver_later_queue_name: 'default'
   layout 'mailer'
 end
