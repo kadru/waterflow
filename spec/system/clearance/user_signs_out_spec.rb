a4d4821a49f970373ba62f9087c19998b3eaa6bc
Oscar <orivas155@gmail.com>
2021-07-01 11:07:06 -0500

    added spec for mobile signout fix

--- spec/system/clearance/user_signs_out_spec.rb
+++ spec/system/clearance/user_signs_out_spec.rb
@@ -3,11 +3,23 @@
 require 'rails_helper'
 require 'support/system/clearance_helpers'
 
-RSpec.feature 'User signs out', type: :system, js: true do
-  scenario 'signs out' do
+RSpec.feature 'User signs out' do
+  scenario 'signs out', type: :system, js: true do
     sign_in
     sign_out
 
     expect_user_to_be_signed_out
+  end
+
+  # Cannot have set two drivers for capybara using driven_by until this PR is merged
+  # https://github.com/rails/rails/pull/42511
+  xcontext 'when is in mobile size', type: :system, mobile: true do
+    scenario 'signs out' do
+      sign_in
+      find('#materialize-select').click
+      sign_out
+
+      expect_user_to_be_signed_out
+    end
   end
 end
