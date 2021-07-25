e30491c641eba48fd0f6a83cd1252e783cdb6adf
Oscar <orivas155@gmail.com>
2021-07-24 23:22:21 -0500

    setup signed and secure cookie

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = ENV.fetch('DEFAULT_SENDER_EMAIL', 'noreply@waterflowapp.com')
   config.rotate_csrf_on_sign_in = true
+  config.secure_cookie = true
+  config.signed_cookie = true
 end
