6b1c49214d4c57816903f05e7af376553fc39a1c
Oscar <orivas155@gmail.com>
2021-11-02 11:59:45 -0600

    refactor application js Using ESM syntax

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,34 +3,28 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-// require("@rails/activestorage").start()
-require("materialize-css")
-// require("channels")
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import M from 'materialize-css';
 
+Turbolinks.start();
+Rails.start();
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-const M = require('materialize-css');
-const datePicker = require('materialize/datePicker')
-const sidenav = require('materialize/sidenav')
-const formSelect = require('materialize/formSelect')
-const floatingActionButton = require('materialize/floatingActionButton')
-const tooltip = require('materialize/tooltip')
 
 let sidenavs;
 let tooltips;
 
 document.addEventListener('turbolinks:load', () => {
-  datePicker.default.initialize();
-  formSelect.default.initialize();
-  sidenavs = sidenav.default.initialize();
-  floatingActionButton.default.initialize();
-  tooltips = tooltip.default.initialize();
+  M.Datepicker.init(document.querySelectorAll('.datepicker'));
+  M.FormSelect.init(document.querySelectorAll('select'));
+  M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'));
+  sidenavs = M.Sidenav.init(document.querySelectorAll('.sidenav'));
+  tooltips = M.Tooltip.init(document.querySelectorAll('.tooltipped'));
   
   // to set active prefilled inputs
   setTimeout(() => {
